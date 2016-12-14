7063f0d3e33055942220d6dffb1de31d7c49a404
Katherine Zaoral <kathy@vauxoo.com>
2016-12-14 14:01:08 -0600

    [ADD] show a difference message when the margin has not been updated.

--- update_sale_order_margin.py
+++ update_sale_order_margin.py
@@ -2,10 +2,14 @@ for sale in obj:
     old_value = sale.margin
     for line in sale.order_line:
         line.write({'purchase_price': line.purchase_price * 1.0})
+    new_value = sale.margin
 
     # Prepare log message
     subject = "Update/Fix Sale Order Margin server action has been run"
-    msg = "<ul><li><b>Margin:</b> %(old)s &#8594; %(new)s </li></ul>" % dict(
-            old=old_value, new=sale.margin)
+    if old_value == new_value:
+        msg = "Nothing to update, the margin is already updated"
+    else:
+        msg = "<ul><li><b>Margin:</b> %(old)s &#8594; %(new)s </li></ul>" % \
+            dict(old=old_value, new=sale.margin)
     self.message_post(cr, uid, object.id, body=msg, type='comment',
                       subject=subject, subtype='html')
