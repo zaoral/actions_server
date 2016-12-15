2fb46d8c02804d02bcdaa01d387873d41b7dfd2a
Katherine Zaoral [Vauxoo] <zaoral@users.noreply.github.com>
2016-12-14 18:41:16 -0600

    New version of server aciton
    - faster compute
    - will only post a message when the margin has been updated.

--- update_sale_order_margin.py
+++ update_sale_order_margin.py
@@ -3,21 +3,17 @@
 # Action To Do: Execute Python Code
 # Click button: Add to "More" menu
 
-sales = self.pool["sale.order"].browse(
-    cr, uid, context["active_ids"], context=context)
-
+sales = self.pool["sale.order"].browse(cr, uid, context["active_ids"], context=context)
 for sale in sales:
     old_value = sale.margin
-    for line in sale.order_line:
-        line.write({'purchase_price': line.purchase_price * 1.0})
+    if not sale.order_line:
+        continue
+    
+    sale.order_line[0].write({'purchase_price': sale.order_line[0].purchase_price * 1.0})
     new_value = sale.margin
 
     # Prepare log message
-    subject = "Update/Fix Sale Order Margin server action has been run"
     if old_value == new_value:
-        msg = "Nothing to update, the margin is already updated"
-    else:
-        msg = "<ul><li><b>Margin:</b> %(old)s &#8594; %(new)s </li></ul>" % \
-            dict(old=old_value, new=sale.margin)
-    self.message_post(cr, uid, sale.id, body=msg, type='comment',
-                      subject=subject, subtype='html')
+        continue
+    msg = "<ul><li><b>Margin:</b> %(old)s &#8594; %(new)s </li></ul>" % dict(old=old_value, new=sale.margin)
+    self.message_post(cr, uid, sale.id, body=msg, type='comment', subject="Update/Fix Sale Order Margin server action has been run", subtype='html')
