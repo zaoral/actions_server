c9f2f25a761466aba68f9daac04532c05231f921
Katherine Zaoral <kathy@vauxoo.com>
2016-12-15 19:32:00 -0600

    [ADD] Fix csv sale order uos qty to compute margin
    When import sale order data from CSV the onchanges that works in the instance
    view ae not called.
    
    In the Instance view, when add a new line and you update the quantity field
    (product_uom_qty) the product_uos_qty is updated with the same value, this
    value even if product_uos_qty is not visible for the user.
    
    The CSV set the product_uom_qty in the line,  but do not set the
    product_uos_qty, When product_uos_qty is not set then the sale order line
    margin is not properly compute.
    
    In order to properly compute the sale order margin and then the sale order
    margin we need to set the product_uos_qty of this sales.

---
+++ fix_csv_sale_order_uos_qty.py
@@ -0,0 +1,33 @@
+# Name:         Fix sale orders imported via CSV: Update UoS Qty needed for compute sale order margin
+# Base Mode:    Sales Order 
+# Action To Do: # Execute Python Code
+# Click button: Add in the 'More' menu
+
+# IMPORTANT: This should be run only one time to fix the error in the import
+# for sale order data using csv file.
+
+# Search messages posted when we fix the sale order margin
+msg_obj = self.pool["mail.message"]
+subject = "Update/Fix sale order margin Server Action has been ran"
+messages = msg_obj.browse(cr, 1, msg_obj.search(cr, 1, [("subject", "ilike", subject), ("model", "=", "sale.order")]))
+
+# Get the sale order browseables related to the posted messages.
+sale_obj = self.pool["sale.order"]
+sales = sale_obj.browse(cr, 1, [msg.res_id for msg in messages])
+
+# TODO delete next lines
+# raise Warning(str(len(sales)) + " " + str(sales))
+sales = self.pool["sale.order"].browse(cr, uid, context["active_ids"], context=context)
+
+for sale in sales:
+    msg = ""
+    for line in sale.order_line:
+        if line.product_uos_qty:
+            continue
+        msg += "<li><b>Line " + str(line.sequence2) + " - UoS Qty:</b> %s &#8594; " % line.product_uos_qty
+        line.write({"product_uos_qty": line.product_uom_qty})
+        msg += str(line.product_uos_qty) + "</li>"
+
+    if not msg:
+        continue
+    self.message_post(cr, uid, sale.id, body="<ul>"+msg+"</ul>", subject="Fix CSV imported sale orders (UoS Qty = UoM Qty) to properly compute sale order margin", type='comment', subtype='html')
