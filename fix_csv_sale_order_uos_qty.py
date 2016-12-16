0dda933c4bcc16ddee440755065e048ee17f6547
Katherine Zaoral <kathy@vauxoo.com>
2016-12-15 20:52:51 -0600

    [ADD] new to make dummy write in one of the sensible sale order line fields in order to recalculate the sale order margin again.

--- fix_csv_sale_order_uos_qty.py
+++ fix_csv_sale_order_uos_qty.py
@@ -20,6 +20,10 @@ sales = sale_obj.browse(cr, 1, [msg.res_id for msg in messages])
 sales = self.pool["sale.order"].browse(cr, uid, context["active_ids"], context=context)
 
 for sale in sales:
+
+    if not sale.order_line:
+        continue
+
     msg = ""
     for line in sale.order_line:
         if line.product_uos_qty:
@@ -30,4 +34,14 @@ for sale in sales:
 
     if not msg:
         continue
-    self.message_post(cr, uid, sale.id, body="<ul>"+msg+"</ul>", subject="Fix CSV imported sale orders (UoS Qty = UoM Qty) to properly compute sale order margin", type='comment', subtype='html')
+
+    # Trigger the changes in one of the lines to update the sale order
+    # margin again to the correct value of the new lines
+    msg_init = "<li><b>Margin:</b> " + str(sale.margin) + " &#8594; "
+    sale.order_line[0].write({'purchase_price': sale.order_line[0].purchase_price * 1.0})
+    msg_init+= str(sale.margin) + "</li>"
+
+    self.message_post(
+        cr, uid, sale.id, type='comment', subtype='html',
+        body="<ul>"+msg_init+msg+"</ul>",
+        subject="Fix CSV imported sale orders (UoS Qty = UoM Qty) to properly compute sale order margin")
